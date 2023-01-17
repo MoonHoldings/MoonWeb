0542375bcfe4610d3213948deb31430ac0f2276a
Abeer <abeer.technology@gmail.com>
2023-01-17 15:28:28 +0600

    signup with email works

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { signupResult } from 'redux/reducers/authSlice'
+import { signup } from 'redux/reducers/authSlice'
 
 const index = () => {
+  const dispatch = useDispatch()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const regResult = useSelector(signupResult)
+  const { status } = useSelector((state) => state.auth)
+
+  const register = () => {
+    dispatch(signup({ email, password }))
+  }
+
+  //=== TIP: you have to add NEXT_PUBLIC_ before any env variable (nextjs rule)
+  useEffect(() => {
+    console.log(
+      'process.env.MOON_SERVER_URL',
+      process.env.NEXT_PUBLIC_MOON_SERVER_URL
+    )
+    console.log(status)
+    console.log(regResult)
+  }, [status])
+
   return (
     <div className="flex flex-col items-center pt-[4.6rem]">
       <h1 className="mt-[4rem] mb-[2rem] text-[3rem] font-bold text-teal-500">
@@ -11,13 +35,20 @@ const index = () => {
           className="mb-[1rem] block text-[1.6rem]"
           type="email"
           placeholder="Enter Email"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           className="block text-[1.6rem]"
           type="password"
           placeholder="Enter Password"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="m-2 bg-teal-600 p-2 text-[1.6rem]">Submit</button>
+        <button
+          onClick={register}
+          className="m-2 bg-teal-600 p-2 text-[1.6rem]"
+        >
+          Submit
+        </button>
       </div>
       <button className="m-2 block bg-indigo-600 p-2 text-[1.6rem]">
         Discord
--- redux/reducers/authSlice.js
+++ redux/reducers/authSlice.js
@@ -1,13 +1,51 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 const initialState = {
   status: 'idle',
+  signupResult: {},
+  error: null,
 }
+
+export const signup = createAsyncThunk('auth/signup', async (creds) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:9000/api/register`,
+      creds,
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+
+    return response.data
+  } catch (error) {
+    return {
+      success: false,
+      message: error,
+    }
+  }
+})
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(signup.pending, (state, action) => {
+        state.status = 'pending'
+      })
+      .addCase(signup.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.signupResult = action.payload
+      })
+      .addCase(signup.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.payload
+      })
+  },
 })
+
+export const signupResult = (state) => state.signupResult
 
 export default authSlice.reducer
