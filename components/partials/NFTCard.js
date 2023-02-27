80a66018c5ffca47504caace8008772ccc0d64cd
leongaban <leongaban@gmail.com>
2023-02-27 14:52:01 +0800

    hide more elements, clean up, added cursor style

--- components/partials/NFTCard.js
+++ components/partials/NFTCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NFTCard = ({ collection }) => {
   return (
-    <div className="rounded-[1rem] border-2 border-[#191C20] bg-[#191C20] p-[1rem] font-inter text-white active:border-[#62EAD2] xl:w-[23.8rem] xl:p-[1.5rem] xl:hover:border-[#62EAD2]">
+    <div className="cursor rounded-[1rem] border-2 border-[#191C20] bg-[#191C20] p-[1rem] font-inter text-white active:border-[#62EAD2] xl:w-[23.8rem] xl:p-[1.5rem] xl:hover:border-[#62EAD2]">
       <img
         className="mb-[1rem] h-[14.75rem] w-full rounded-[1rem] object-cover xl:mb-[1.5rem] xl:h-[20.08rem]"
         src={collection.image}
@@ -11,7 +11,6 @@ const NFTCard = ({ collection }) => {
       <div className="details">
         <div className="xl:mb-[1.2rem] xl:flex xl:justify-between">
           <h1 className="mb-[0.4rem] text-[1.2rem] font-bold leading-[1.5rem] xl:mb-0 xl:text-[1.4rem]">
-            {/* DeGods */}
             {collection.name}
           </h1>
           <h2 className="mb-[0.4rem] text-[1.2rem] font-semibold leading-[1.5rem] text-[#62EAD2] xl:mb-0">
@@ -19,7 +18,7 @@ const NFTCard = ({ collection }) => {
           </h2>
         </div>
 
-        <div className="xl:flex xl:justify-between">
+        {/* <div className="xl:flex xl:justify-between">
           <div className="mb-[0.4rem] flex items-center text-[1.2rem] font-semibold leading-[1.5rem] xl:mb-0 xl:text-[1.8rem]">
             <div className="">48,200</div>
             <img
@@ -31,7 +30,7 @@ const NFTCard = ({ collection }) => {
           <div className="mb-[0.4rem] text-[1.2rem] xl:mb-0 xl:text-[1.8rem] xl:font-light xl:leading-[1.5rem]">
             $482,000
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
