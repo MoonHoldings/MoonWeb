8626878058b261ad076693863fe99dd43a2c9e75
ralphbibera <125718293+artofkaiju@users.noreply.github.com>
2023-03-11 21:55:07 +0800

    Added Square Ratio For Images

--- components/partials/Collections.js
+++ components/partials/Collections.js
@@ -14,7 +14,7 @@ const Collections = ({ collections }) => {
         <u>{totalNftCount}</u> NFTs
       </p>
 
-      <div className="grid grid-cols-2 gap-4 xl:grid-cols-3 2xl:grid-cols-6 3xl:grid-cols-9 4xl:grid-cols-12">
+      <div className="grid grid-cols-2 gap-6 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-6 4xl:grid-cols-8 h">
         {/* <div className="nft-cards grid grid-cols-2 gap-x-[2rem] gap-y-[2rem] sm:grid-cols-3 sm:gap-x-[1.3rem] sm:gap-y-[1.5rem] xl:grid-cols-3"> */}
         {/* {[1, 2, 3, 4, 5].map((card) => (
     <CollectionCard key={card} />
