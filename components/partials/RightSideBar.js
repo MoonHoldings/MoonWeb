9665b15f39635707fdb3b97e75d29c8264e0e1ae
Allen Vincent Gonzales <vincegonzalesdlsu@gmail.com>
2023-03-15 11:41:17 +0800

    Update hover style of disconnect wallet

--- components/partials/RightSideBar.js
+++ components/partials/RightSideBar.js
@@ -364,7 +364,7 @@ const RightSideBar = () => {
 
           <div
             onClick={disconnectWallets}
-            className="flex h-[6.4rem] cursor-pointer items-center justify-between rounded-[1rem] border border-black bg-[#25282C] px-[1.6rem]"
+            className="xl-[1rem] flex h-[6.4rem] cursor-pointer items-center justify-between rounded-[1rem] border border-black bg-[#25282C] px-[1.6rem] text-white hover:border-teal-400 hover:text-teal-400"
           >
             <div className="flex h-[4.1rem] w-full items-center text-[1.4rem] text-[#FFFFFF]">
               <Image
