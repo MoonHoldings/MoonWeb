91c5bc896ef30a979d12b35ab33209f466168166
abeertech01 <abeer.technology@gmail.com>
2023-03-22 21:12:34 +0600

    bar chart developed

--- components/partials/CryptoSquare.js
+++ components/partials/CryptoSquare.js
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 
-const CryptoSquare = () => {
+const CryptoSquare = ({ crypto }) => {
+  const totalValue = (holding, price) => {
+    const value = holding * price
+    return value.toLocaleString()
+  }
   return (
     <div className="cursor xl:min- relative flex min-h-min flex-col rounded-[1rem] border-2 border-[#191C20] bg-[#191C20] p-[1rem] font-inter text-white active:border-[#62EAD2] xl:min-h-[23.8rem] xl:max-w-[23.8rem]  xl:hover:border-[#62EAD2]">
       <div className="headline mb-[1.6rem] flex gap-[.6rem]">
@@ -11,13 +15,15 @@ const CryptoSquare = () => {
           height={17}
           alt="btc"
         />
-        <span className="text-[1.4rem]">Bitcoin</span>
+        <span className="text-[1.4rem]">{crypto.name}</span>
       </div>
-      <div className="value-box absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
+      <div className="value-box absolute top-[50%] left-[50%] flex -translate-x-[50%] -translate-y-[50%] flex-col items-center">
         <h1 className="quantity text-[7.5rem] font-[700] leading-[9.07rem]">
-          54
+          {crypto.holding}
         </h1>
-        <div className="value-dollar text-[2.2rem] font-[500]">$345,987</div>
+        <div className="value-dollar text-[2.2rem] font-[500]">
+          ${totalValue(crypto.holding, crypto.price)}
+        </div>
         <div className="pct-dollar flex justify-center">
           <div className="mr-2 text-[#62EAD2]">+0.45%</div>
           <div>$16,823.23</div>
--- pages/crypto/index.js
+++ pages/crypto/index.js
@@ -2,16 +2,121 @@ import React from 'react'
 import { CRYPTO_PORTFOLIO } from 'app/constants/copy'
 import SidebarsLayout from 'components/nft/SidebarsLayout'
 import CryptoSquare from 'components/partials/CryptoSquare'
+import { color } from 'framer-motion'
 
 const index = () => {
+  const dummyCryptos = [
+    {
+      name: 'Bitcoin',
+      id: 'BTC',
+      holding: 6,
+      price: 24000,
+      colors: {
+        text: '#000',
+        background: ['#F7931A', '#FFC783'],
+      },
+    },
+    {
+      name: 'Ethereum',
+      id: 'ETH',
+      holding: 15,
+      price: 1700,
+      colors: {
+        text: '#fff',
+        background: ['#761FC3', '#19012F'],
+      },
+    },
+    {
+      name: 'Solana',
+      id: 'SOL',
+      holding: 800,
+      price: 24,
+      colors: {
+        text: '#fff',
+        background: ['#5B218F', '#DB00FF'],
+      },
+    },
+    {
+      name: 'Tether',
+      id: 'USDT',
+      holding: 7000,
+      price: 1,
+      colors: {
+        text: '#fff',
+        background: ['#53AE94'],
+      },
+    },
+    {
+      name: 'Polkadot',
+      id: 'DOT',
+      holding: 6000,
+      price: 6.4,
+      colors: {
+        text: '#fff',
+        background: ['#E6007A'],
+      },
+    },
+    {
+      name: 'Litecoin',
+      id: 'LTC',
+      holding: 25,
+      price: 80,
+      colors: {
+        text: '#000',
+        background: ['#BEBEBE'],
+      },
+    },
+  ]
+
+  // const totalValue = dummyCryptos.sort()
+
+  const pct = (holding, price) => {
+    let valueSum = 0
+    dummyCryptos.forEach((crypto) => {
+      valueSum += crypto.holding * crypto.price
+    })
+
+    const thePct = (holding * price * 100) / valueSum
+
+    return thePct
+  }
   return (
     <SidebarsLayout>
       <div className="text-white md:order-2">
-        <h1 className="text-[2.8rem]">{CRYPTO_PORTFOLIO}</h1>
+        <h1 className="mb-[2.6rem] text-[2.8rem]">{CRYPTO_PORTFOLIO}</h1>
+        <div className="barchart flex">
+          {dummyCryptos
+            .sort((a, b) => b.holding * b.price - a.holding * a.price)
+            .map((crypto, index) => (
+              <div
+                key={index}
+                className="flex h-[4.8rem] items-center"
+                style={{
+                  background: crypto.colors.background[0],
+                  color: crypto.colors.text,
+                  width: pct(crypto.holding, crypto.price) + '%',
+                  borderRight:
+                    index !== dummyCryptos.length - 1 && '0.01px solid #000000',
+                  borderTopLeftRadius: index === 0 && '0.5rem',
+                  borderTopRightRadius:
+                    index === dummyCryptos.length - 1 && '0.5rem',
+                  borderBottomRightRadius:
+                    index === dummyCryptos.length - 1 && '0.5rem',
+                  borderBottomLeftRadius: index === 0 && '0.5rem',
+                }}
+              >
+                <span className="ml-[1rem] text-[1.4rem]">
+                  {pct(crypto.holding, crypto.price) > 8 && crypto.id}
+                </span>
+              </div>
+            ))}
+        </div>
         <div className="nft-cards mt-[2rem] grid grid-cols-2 gap-x-[2rem] gap-y-[2rem] sm:grid-cols-3 sm:gap-x-[1.3rem] sm:gap-y-[1.5rem] xl:grid-cols-3">
-          {[1, 2, 3, 4, 5, 6].map((num) => (
-            <CryptoSquare key={num} />
-          ))}
+          {dummyCryptos
+            .sort((a, b) => b.holding * b.price - a.holding * a.price)
+            .map((crypto, index) => (
+              <CryptoSquare key={index} crypto={crypto} />
+            ))}
         </div>
       </div>
     </SidebarsLayout>
