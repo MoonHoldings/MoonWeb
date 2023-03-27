d6d21c8265a3cb9730e94b5970d4f9bc2469781d
Allen Vincent Gonzales <vincegonzalesdlsu@gmail.com>
2023-03-27 13:21:02 +0800

    Add total sol and usd total value in collections page

--- pages/nfts/collection/index.js
+++ pages/nfts/collection/index.js
@@ -33,6 +33,23 @@ const Index = () => {
     )}`
   }
 
+  const formatFloorPriceTotal = () => {
+    return toCurrencyFormat(
+      (parseFloat(currentCollection.floorPrice.floorPriceLamports) /
+        LAMPORTS_PER_SOL) *
+        currentCollection?.nfts?.length
+    )
+  }
+
+  const formatFloorPriceUsdTotal = () => {
+    return `$${toCurrencyFormat(
+      (parseFloat(currentCollection.floorPrice.floorPriceLamports) /
+        LAMPORTS_PER_SOL) *
+        solUsdPrice *
+        currentCollection?.nfts?.length
+    )}`
+  }
+
   return (
     <SidebarsLayout>
       <div className="mt-[2rem] text-white md:order-2">
@@ -53,22 +70,48 @@ const Index = () => {
           {currentCollection.name} NFTs
         </p>
         {currentCollection.floorPrice && (
-          <div className="mt-8 flex items-center">
-            <p className="bold text-[2rem]">
-              Floor Price: {formatFloorPrice()}
-            </p>
-            <Image
-              className="ml-2 inline h-[1.5rem] w-[1.5rem] xl:h-[2rem] xl:w-[2rem]"
-              src="/images/svgs/sol-symbol.svg"
-              alt="SOL Symbol"
-              width={0}
-              height={0}
-              unoptimized
-            />
-            <TextBlink
-              className="bold ml-4 text-[2rem]"
-              text={formatFloorPriceUsd()}
-            />
+          <div className="mt-8 flex items-center justify-between">
+            <div className="flex items-center">
+              <div className="bold flex items-center text-center text-[1.5rem] md:text-[2rem]">
+                Floor Price:
+                <div className="ml-4 flex items-center">
+                  {formatFloorPrice()}
+                  <Image
+                    className="ml-2 inline h-[1.5rem] w-[1.5rem] xl:h-[2rem] xl:w-[2rem]"
+                    src="/images/svgs/sol-symbol.svg"
+                    alt="SOL Symbol"
+                    width={0}
+                    height={0}
+                    unoptimized
+                  />
+                </div>
+              </div>
+              <TextBlink
+                className="bold ml-4 text-[1.5rem] md:text-[2rem]"
+                text={formatFloorPriceUsd()}
+              />
+            </div>
+
+            <div className="flex items-center">
+              <div className="bold flex items-center text-center text-[1.5rem] md:text-[2rem]">
+                Total Value:
+                <div className="ml-4 flex items-center">
+                  {formatFloorPriceTotal()}
+                  <Image
+                    className="ml-2 inline h-[1.5rem] w-[1.5rem] xl:h-[2rem] xl:w-[2rem]"
+                    src="/images/svgs/sol-symbol.svg"
+                    alt="SOL Symbol"
+                    width={0}
+                    height={0}
+                    unoptimized
+                  />
+                </div>
+              </div>
+              <TextBlink
+                className="bold ml-4 text-[1.5rem] md:text-[2rem]"
+                text={formatFloorPriceUsdTotal()}
+              />
+            </div>
           </div>
         )}
         <div className="h grid grid-cols-2 gap-6 py-[2rem] xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-6 4xl:grid-cols-8">
