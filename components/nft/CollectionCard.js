329e24a1b94b2484fdb253313c490e258d4d6bcc
Allen Vincent Gonzales <vincegonzalesdlsu@gmail.com>
2023-03-30 13:58:04 +0800

    Fix import and yarn lock

--- components/nft/CollectionCard.js
+++ components/nft/CollectionCard.js
@@ -14,6 +14,7 @@ import {
 import { HELLO_MOON_URL, AXIOS_CONFIG_HELLO_MOON_KEY } from 'app/constants/api'
 import toCurrencyFormat from 'utils/toCurrencyFormat'
 import toShortCurrencyFormat from 'utils/toShortCurrencyFormat'
+import isShortCurrencyFormat from 'utils/isShortCurrencyFormat'
 import TextBlink from './TextBlink'
 
 const CollectionCard = ({ collection, index }) => {
