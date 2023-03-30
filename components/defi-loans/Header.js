e03f5b1f880199898fd10682930d39fbe3b55093
Allen Vincent Gonzales <vincegonzalesdlsu@gmail.com>
2023-03-30 15:18:18 +0800

    Fix classnames

--- components/defi-loans/Header.js
+++ components/defi-loans/Header.js
@@ -2,9 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import mergeClasses from 'utils/mergeClasses'
 
 const Header = ({ title, description }) => {
   const router = useRouter()
+
+  const isLend = router.pathname.includes('lend')
+  const isBorrow = router.pathname.includes('borrow')
 
   return (
     <>
@@ -13,17 +17,39 @@ const Header = ({ title, description }) => {
         <div className="rounded-[30px] bg-[#191C20] p-4">
           <Link
             href="lend"
-            className={`inline-flex items-center justify-center rounded-[30px] border border-transparent bg-[${
-              router.pathname.includes('lend') && '#3C434B'
-            }] py-1.5 px-4 text-[1.4rem] text-white focus:outline-none`}
+            className={mergeClasses(
+              'inline-flex',
+              'items-center',
+              'justify-center',
+              'rounded-[30px]',
+              'border',
+              'border-transparent',
+              'py-1.5',
+              'px-4',
+              'text-[1.4rem]',
+              'text-white',
+              'focus:outline-none',
+              isLend && 'bg-[#3C434B]'
+            )}
           >
             Lend
           </Link>
           <Link
             href="borrow"
-            className={`inline-flex items-center justify-center rounded-[30px] border border-transparent bg-[${
-              router.pathname.includes('borrow') && '#3C434B'
-            }] py-1.5 px-4 text-[1.4rem] text-white focus:outline-none`}
+            className={mergeClasses(
+              'inline-flex',
+              'items-center',
+              'justify-center',
+              'rounded-[30px]',
+              'border',
+              'border-transparent',
+              'py-1.5',
+              'px-4',
+              'text-[1.4rem]',
+              'text-white',
+              'focus:outline-none',
+              isBorrow && 'bg-[#3C434B]'
+            )}
           >
             Borrow
           </Link>
