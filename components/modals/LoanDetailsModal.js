0f6e4fae53d9cc44add64f971b113e47604c53ad
Vince G <allen_gonzales@dlsu.edu.ph>
2023-04-06 20:34:01 +0800

    Refactor loan details modal

--- components/modals/LoanDetailsModal.js
+++ components/modals/LoanDetailsModal.js
@@ -19,6 +19,102 @@ const LoanDetailsModal = () => {
     loanDetails?.loan?.totalOfferedLoans -
     loanDetails?.loan?.latestOfferedLoans?.length
 
+  const formatElapsedTime = (unixTime) => {
+    const currentUnixTime = Math.floor(Date.now() / 1000)
+    let timePassed
+    let unit
+
+    const secondsPassed = currentUnixTime - unixTime
+
+    if (secondsPassed < 60) {
+      timePassed = secondsPassed
+      unit = 'second'
+    } else if (secondsPassed < 3600) {
+      timePassed = Math.floor(secondsPassed / 60)
+      unit = 'minute'
+    } else if (secondsPassed < 86400) {
+      timePassed = Math.floor(secondsPassed / 3600)
+      unit = 'hour'
+    } else if (secondsPassed < 2592000) {
+      timePassed = Math.floor(secondsPassed / 86400)
+      unit = 'day'
+    } else {
+      timePassed = Math.floor(secondsPassed / 2592000)
+      unit = 'month'
+    }
+
+    if (timePassed !== 1) {
+      unit += 's'
+    }
+
+    return `${timePassed} ${unit} ago`
+  }
+
+  const renderTakenLoans = () => {
+    return loanDetails?.loan?.latestTakenLoans?.map((loan, index) => {
+      return (
+        <div
+          className="flex justify-around border-b border-b-white border-opacity-25"
+          key={index}
+        >
+          <div className="flex flex-1 justify-center bg-[#101113] py-5 text-[1.6rem]">
+            <div className="flex items-center">
+              {toCurrencyFormat(loan.principalLamports / LAMPORTS_PER_SOL)}
+              <Image
+                className="ml-3 h-[1.7rem] w-[1.7rem]"
+                src="/images/svgs/sol.svg"
+                width={24}
+                height={24}
+                alt=""
+              />
+            </div>
+            <div className="mx-6">{'>'}</div>
+            <div className="flex items-center">
+              {toCurrencyFormat(loan.totalOwedLamports / LAMPORTS_PER_SOL)}
+              <Image
+                className="ml-3 h-[1.7rem] w-[1.7rem]"
+                src="/images/svgs/sol.svg"
+                width={24}
+                height={24}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="flex flex-1 justify-center bg-[#212327] py-5 text-[1.6rem]">
+            {formatElapsedTime(loan.takenTime)}
+          </div>
+        </div>
+      )
+    })
+  }
+
+  const renderOffers = () => {
+    return loanDetails?.loan?.latestOfferedLoans?.map((loan, index) => {
+      return (
+        <div
+          className="flex justify-around border-b border-b-white border-opacity-25"
+          key={index}
+        >
+          <div className="flex flex-1 justify-center bg-[#101113] py-5 text-[1.6rem]">
+            <div className="flex items-center">
+              {toCurrencyFormat(loan.principalLamports / LAMPORTS_PER_SOL)}
+              <Image
+                className="ml-3 h-[1.7rem] w-[1.7rem]"
+                src="/images/svgs/sol.svg"
+                width={24}
+                height={24}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="flex flex-1 justify-center bg-[#212327] py-5 text-[1.6rem]">
+            {formatElapsedTime(loan.offerTime)}
+          </div>
+        </div>
+      )
+    })
+  }
+
   return (
     loanDetailsModalOpen && (
       <motion.div
@@ -56,75 +152,7 @@ const LoanDetailsModal = () => {
               Taken
             </div>
           </div>
-          <div className="flex flex-col ">
-            {loanDetails?.loan?.latestTakenLoans?.map((loan, index) => {
-              const currentUnixTime = Math.floor(Date.now() / 1000)
-              const takenTime = loan.takenTime
-              let timePassed
-              let unit
-
-              const secondsPassed = currentUnixTime - takenTime
-
-              if (secondsPassed < 60) {
-                timePassed = secondsPassed
-                unit = 'second'
-              } else if (secondsPassed < 3600) {
-                timePassed = Math.floor(secondsPassed / 60)
-                unit = 'minute'
-              } else if (secondsPassed < 86400) {
-                timePassed = Math.floor(secondsPassed / 3600)
-                unit = 'hour'
-              } else if (secondsPassed < 2592000) {
-                timePassed = Math.floor(secondsPassed / 86400)
-                unit = 'day'
-              } else {
-                timePassed = Math.floor(secondsPassed / 2592000)
-                unit = 'month'
-              }
-
-              if (timePassed !== 1) {
-                unit += 's'
-              }
-
-              return (
-                <div
-                  className="flex justify-around border-b border-b-white border-opacity-25"
-                  key={index}
-                >
-                  <div className="flex flex-1 justify-center bg-[#101113] py-5 text-[1.6rem]">
-                    <div className="flex items-center">
-                      {toCurrencyFormat(
-                        loan.principalLamports / LAMPORTS_PER_SOL
-                      )}
-                      <Image
-                        className="ml-3 h-[1.7rem] w-[1.7rem]"
-                        src="/images/svgs/sol.svg"
-                        width={24}
-                        height={24}
-                        alt=""
-                      />
-                    </div>
-                    <div className="mx-6">{'>'}</div>
-                    <div className="flex items-center">
-                      {toCurrencyFormat(
-                        loan.totalOwedLamports / LAMPORTS_PER_SOL
-                      )}
-                      <Image
-                        className="ml-3 h-[1.7rem] w-[1.7rem]"
-                        src="/images/svgs/sol.svg"
-                        width={24}
-                        height={24}
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-1 justify-center bg-[#212327] py-5 text-[1.6rem]">
-                    {timePassed} {unit} ago
-                  </div>
-                </div>
-              )
-            })}
-          </div>
+          <div className="flex flex-col ">{renderTakenLoans()}</div>
           {moreTakenLoansCount > 0 && (
             <div className="flex w-full justify-center bg-[#101113] py-6 text-[1.4rem]">
               More ({moreTakenLoansCount})
@@ -151,63 +179,7 @@ const LoanDetailsModal = () => {
               Offer Made
             </div>
           </div>
-          <div className="flex flex-col">
-            {loanDetails?.loan?.latestOfferedLoans?.map((loan, index) => {
-              const currentUnixTime = Math.floor(Date.now() / 1000)
-              const offerTime = loan.offerTime
-              let timePassed
-              let unit
-
-              const secondsPassed = currentUnixTime - offerTime
-
-              if (secondsPassed < 60) {
-                timePassed = secondsPassed
-                unit = 'second'
-              } else if (secondsPassed < 3600) {
-                timePassed = Math.floor(secondsPassed / 60)
-                unit = 'minute'
-              } else if (secondsPassed < 86400) {
-                timePassed = Math.floor(secondsPassed / 3600)
-                unit = 'hour'
-              } else if (secondsPassed < 2592000) {
-                timePassed = Math.floor(secondsPassed / 86400)
-                unit = 'day'
-              } else {
-                timePassed = Math.floor(secondsPassed / 2592000)
-                unit = 'month'
-              }
-
-              if (timePassed !== 1) {
-                unit += 's'
-              }
-
-              return (
-                <div
-                  className="flex justify-around border-b border-b-white border-opacity-25"
-                  key={index}
-                >
-                  <div className="flex flex-1 justify-center bg-[#101113] py-5 text-[1.6rem]">
-                    <div className="flex items-center">
-                      {toCurrencyFormat(
-                        loan.principalLamports / LAMPORTS_PER_SOL
-                      )}
-                      <Image
-                        className="ml-3 h-[1.7rem] w-[1.7rem]"
-                        src="/images/svgs/sol.svg"
-                        width={24}
-                        height={24}
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-1 justify-center bg-[#212327] py-5 text-[1.6rem]">
-                    {timePassed} {unit} ago
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-
+          <div className="flex flex-col">{renderOffers()}</div>
           {moreActiveOffersCount > 0 ? (
             <div className="flex w-full justify-center rounded-b-[1.25rem] bg-[#101113] py-6 text-[1.4rem]">
               More ({moreActiveOffersCount})
