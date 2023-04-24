03415ba705f2328210c30877292ac9b56ddb21fa
micohalvarez <micohalvarez@yahoo.com>
2023-04-24 15:06:00 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,7 +10,6 @@ export const authOptions = {
     CredentialsProvider({
       // The name to display on the sign in form (e.g. 'Sign in with...')
       name: 'Credentials',
-
       // The credentials is used to generate a suitable form on the sign in page.
       // You can specify whatever fields you are expecting to be submitted.
       // e.g. domain, username, password, 2FA token, etc.
@@ -41,7 +40,6 @@ export const authOptions = {
           if (user) {
             return { email: user.email, jid: user.accessToken }
           }
-
           return null
         }
       },
@@ -54,8 +52,7 @@ export const authOptions = {
       return token
     },
     session: async ({ session, token }) => {
-      session.accessToken = token.accessToken // Setting token in session
-      return session
+      return { ...session, user: { email: token.email } }
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
