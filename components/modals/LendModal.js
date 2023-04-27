bae226a088fc02ca05c04e907e4b708986715063
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-04-27 13:13:14 +0800

    Show notification for offer and borrow

--- components/modals/LendModal.js
+++ components/modals/LendModal.js
@@ -5,7 +5,7 @@ import { FormProvider, useForm } from 'react-hook-form'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Alert } from 'antd'
+import { Alert, notification } from 'antd'
 import { useWallet } from '@solana/wallet-adapter-react'
 import createAnchorProvider, { connection } from 'utils/createAnchorProvider'
 import { LAMPORTS_PER_SOL } from '@solana/web3.js'
@@ -89,6 +89,28 @@ const LendModal = () => {
       getBalance()
       setIsSuccess(true)
       setTxLink(`https://solana.fm/tx/${tx}?cluster=mainnet-qn1`)
+      notification.open({
+        closeIcon: (
+          <svg
+            className="ml-2"
+            viewBox="64 64 896 896"
+            focusable="false"
+            data-icon="close"
+            width="0.9em"
+            height="0.9em"
+            fill="#ffffff"
+            aria-hidden="true"
+          >
+            <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path>
+          </svg>
+        ),
+        type: 'success',
+        className: 'bg-[#191C20] text-white',
+        description: `Done! You've offered a ${orderBook?.duration} day loan, in a few seconds this will reflect in your history`,
+        duration: 60,
+        placement: 'top',
+        message: <p className="text-white">Success</p>,
+      })
     }
   }
 
