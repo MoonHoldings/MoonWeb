27e3eed3ac0e22caecb2d8abef58c4a9712afd17
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-05-22 17:31:27 +0800

    Sort collections

--- components/nft/Collections.js
+++ components/nft/Collections.js
@@ -7,6 +7,11 @@ const Collections = ({ collections }) => {
     (total, col) => total + col.nfts?.length,
     0
   )
+
+  const sortedCollections = [...collections]?.sort(
+    (a, b) => b?.floorPrice * b?.nfts?.length - a?.floorPrice * a?.nfts?.length
+  )
+
   return (
     <div className="nft-portfolio mt-[2rem] text-white md:order-2">
       <h1 className="text-[2.9rem]">{NFT_PORTFOLIO}</h1>
@@ -16,7 +21,7 @@ const Collections = ({ collections }) => {
       </p>
 
       <div className="h grid grid-cols-2 gap-6 py-[2rem] xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-6 4xl:grid-cols-8">
-        {collections?.map((col, index) => (
+        {sortedCollections?.map((col, index) => (
           <CollectionCard key={index} index={index} collection={col} />
         ))}
       </div>
