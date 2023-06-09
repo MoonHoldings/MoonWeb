13a69e0e05fe4d2b566b1a8747290059620408c2
finnius25 <finneykoshy@gmail.com>
2023-06-09 16:07:09 -0500

    Write Homepage tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -32,28 +32,62 @@
 //   })
 // })
 
-describe('Login', () => {
-  const email = Cypress.env('email')
-  const password = Cypress.env('password')
-
-  before(() => {
-    cy.visit('http://localhost:3000')
+describe('Homepage Test', () => {
+  beforeEach(() => {
+    // Visit homepage before each test
+    cy.visit('http://localhost:3000', { timeout: 10000 })
   })
 
-  it('Navigate to login', () => {
-    cy.contains('MoonHoldings').should('be.visible')
-    cy.get('button').should('be.visible').contains('Login').click()
+  it('Loads successfully', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
   })
 
-  it('Should successfully login', () => {
-    cy.visit('http://localhost:3000/login')
-    cy.get('input[type=email').type(email)
-    cy.get('input[type=password').type(password)
-    cy.get('button').contains('Login').click()
+  it('has working Signup link', () => {
+    // Find the Signup button and click it
+    cy.contains('button', 'Signup', { timeout: 5000 }).click()
 
-    cy.wait(10000)
-    cy.url().should('include', 'http://localhost:3000/dashboard')
-    cy.contains('Performance').should('be.visible')
-    cy.contains('Total Networth').should('be.visible')
+    // Check if it redirects to the correct page
+    cy.url().should('eq', 'http://localhost:3000/signup')
+  })
+
+  it('has working Login link', () => {
+    // Go back to homepage
+    cy.visit('http://localhost:3000', { timeout: 10000 })
+
+    // Find the Login button and click it
+    cy.contains('button', 'Login', { timeout: 5000 }).click()
+
+    // Check if it redirects to the correct page
+    cy.url().should('eq', 'http://localhost:3000/login')
   })
 })
+
+// describe('Login', () => {
+//   const email = Cypress.env('email')
+//   const password = Cypress.env('password')
+
+//   before(() => {
+//     cy.visit('http://localhost:3000', { timeout: 10000 })
+//   })
+
+//   it('Navigate to login', () => {
+//     cy.contains('MoonHoldings', { timeout: 10000 })
+//     cy.get('button').contains('Login').click()
+//   })
+
+//   it('Should successfully login', () => {
+//     cy.visit('http://localhost:3000/login')
+//     cy.get('input[type=email').type(email)
+//     cy.get('input[type=password').type(password)
+//     cy.get('button').contains('Login').click()
+
+//     cy.wait(10000)
+//     cy.url().should('include', 'http://localhost:3000/dashboard')
+//   })
+
+//   it('Dashboard', () => {
+//     cy.visit('http://localhost:3000/dashboard')
+//     cy.contains('Performance')
+//     cy.contains('Total Networth')
+//   })
+// })
