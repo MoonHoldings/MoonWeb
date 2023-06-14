bfadf38ddc7f63dd3d3ec08a4b5703a25ef41185
micohalvarez <micohalvarez@yahoo.com>
2023-06-14 21:31:16 +0800

    Mainnet hotfix

--- contexts/ContextProvider.js
+++ contexts/ContextProvider.js
@@ -18,7 +18,7 @@ import { useMemo } from 'react'
 import { Get } from '../utils/solanaAdapterNetwork'
 
 const WalletContextProvider = ({ children }) => {
-  const network = 'devnet'
+  const network = 'mainnet-beta'
   const endpoint = useMemo(() => Get.clusterUrl(network), [network])
 
   const wallets = useMemo(
