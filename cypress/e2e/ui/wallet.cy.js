0ef2d550190a97d9dd3acc3917cc925b4462ea0a
finnius25 <finneykoshy@gmail.com>
2023-06-15 22:20:39 -0500

    update cypress wallet tests

--- cypress/e2e/ui/wallet.cy.js
+++ cypress/e2e/ui/wallet.cy.js
@@ -3,6 +3,7 @@ describe('Wallet', () => {
   const apiGraphQL = 'http://localhost/graphql'
   const email = 'test@email.com'
   const password = 'testPassword'
+  const testWalletAddress = '3bpETYgcq63kWs86BEnWM4QiBUisfP8BrmjHYGjsHL11'
 
   beforeEach(() => {
     cy.visit(`${baseUrl}/login`)
@@ -36,13 +37,13 @@ describe('Wallet', () => {
           data: {
             getUserDashboard: {
               crypto: {
-                total: 10,
-                percentChange: -90,
+                total: 1584021,
+                percentChange: 87,
                 __typename: 'UserDashboardData',
               },
               nft: {
-                total: 0,
-                percentChange: -100,
+                total: 20258,
+                percentChange: 47,
                 __typename: 'UserDashboardData',
               },
               loan: {
@@ -55,59 +56,59 @@ describe('Wallet', () => {
                 percentChange: 0,
                 __typename: 'UserDashboardData',
               },
-              percentChangeTotal: -100,
+              percentChangeTotal: 134,
               __typename: 'UserDashboardResponse',
             },
           },
         })
+      }
 
-        if (
-          req.body.operationName === 'RemoveUserWallet' &&
-          req.body.variables.wallet === '12345678'
-        ) {
-          req.alias = 'gqlRemoveWallet'
-          req.reply({
-            data: {
-              removeUserWallet: true,
-            },
-          })
-        }
+      if (
+        req.body.operationName === 'Mutation' &&
+        req.body.query.includes('removeAllUserWallets')
+      ) {
+        req.alias = 'gqlRemoveAllWallets'
+        req.headers['Authorization'] = 'Bearer fakeToken'
+        req.reply({
+          data: {
+            removeAllUserWallets: true,
+          },
+        })
+      }
 
-        if (
-          req.body.operationName === 'AddUserWallet' &&
-          req.body.variables.wallet === '12345678' &&
-          req.body.variables.verified === true
-        ) {
-          req.alias = 'gqlAddWallet'
-          req.reply({
-            data: {
-              addUserWallet: true,
-            },
-          })
-        }
+      if (
+        req.body.operationName === 'AddUserWallet' &&
+        req.body.variables.wallet === testWalletAddress
+      ) {
+        req.alias = 'gqlAddWallet'
+        req.reply({
+          data: {
+            addUserWallet: true,
+          },
+        })
+      }
 
-        if (
-          req.body.operationName === 'GetUserWallets' &&
-          req.body.variables.type === 'Auto'
-        ) {
-          req.alias = 'gqlGetWallet'
-          req.reply({
-            data: {
-              getUserWallets: [
-                {
-                  id: '1',
-                  address: '12345678',
-                  verified: true,
-                  __typename: 'Wallet',
-                },
-                // add more wallet objects if needed
-              ],
-            },
-          })
-        }
+      if (
+        req.body.operationName === 'GetUserWallets' &&
+        req.body.variables.type === 'Auto'
+      ) {
+        req.alias = 'gqlGetWallet'
+        req.reply({
+          data: {
+            getUserWallets: [
+              {
+                id: '1',
+                address: testWalletAddress,
+                verified: true,
+                __typename: 'Wallet',
+              },
+              // add more wallet objects if needed
+            ],
+          },
+        })
       }
     })
-    cy.setCookie('aid', 'test-cookie')
+    cy.setCookie('aid', 'test-aid-cookie')
   })
 
   it('should add a wallet', () => {
@@ -115,7 +116,17 @@ describe('Wallet', () => {
     // cy.login(email, password)
     cy.get('button#btn-wallet-mobile').click()
     cy.get('button').contains('Add Wallet Address').click()
-    cy.get('div.search').type('12345678')
+    cy.get('div.search').type(testWalletAddress)
     cy.get('button#btn-add-wallet').click()
+    cy.get('ul.all-wallets').contains('3bpET').should('exist')
   })
+
+  // NFT TESTS NEED TO BE COMPLETED FIRST
+  // it('should remove all wallets', () => {
+  //   cy.visit(`${baseUrl}/login`)
+  //   // cy.login(email, password)
+  //   cy.get('button#btn-wallet-mobile').click()
+  //   cy.contains('Disconnect Wallets').click()
+  //   cy.contains('3bpET').should('not.exist')
+  // })
 })
