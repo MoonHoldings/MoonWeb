6f1d61b06e669c6f480552ddcd0ed34d034542e9
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-06-28 12:33:02 +0800

    Bring back historical loans features

--- components/defi-loans/HistoryRow.js
+++ components/defi-loans/HistoryRow.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import mergeClasses from 'utils/mergeClasses'
 
-const HistoryRow = ({ history }) => {
+const HistoryRow = ({ history, isLendPage }) => {
   return (
     <tr
       className={mergeClasses(
@@ -44,7 +44,11 @@ const HistoryRow = ({ history }) => {
       </td>
       <td className="px-6 py-6">
         <div className="flex items-center">
-          <p>{history.offerInterest.toFixed(4)}</p>{' '}
+          <p>
+            {isLendPage
+              ? history.offerInterest.toFixed(4)
+              : history.borrowInterest.toFixed(4)}
+          </p>{' '}
           <Image
             className="ml-3 h-[1.6rem] w-[1.6rem]"
             src="/images/svgs/sol.svg"
